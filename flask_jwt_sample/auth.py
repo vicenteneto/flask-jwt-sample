8cb5c79c6319a53ec0f2789ff880c9c091cbcadc
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-03-01 23:42:08 -0300

    Refactoring configure_jwt function to use annotations

--- flask_jwt_sample/auth.py
+++ flask_jwt_sample/auth.py
@@ -4,16 +4,18 @@ from flask_jwt_sample.users.models import User
 from werkzeug.security import safe_str_cmp
 
 
-def authenticate(username, password):
-    user = services.get_user_by_username(username)
-    if user and safe_str_cmp(user['password'].encode('utf-8'), password.encode('utf-8')):
-        return User(user['name'], user['email'], user['username'], user['password'])
-
-
-def identify(payload):
-    user_id = payload['identity']
-    return services.get_user_by_username(user_id)
-
-
 def configure_jwt(flask_app):
-    JWT(flask_app, authenticate, identify)
+    jwt_app = JWT()
+
+    @jwt_app.authentication_handler
+    def authenticate(username, password):
+        user = services.get_user_by_username(username)
+        if user and safe_str_cmp(user['password'].encode('utf-8'), password.encode('utf-8')):
+            return User(user['name'], user['email'], user['username'], user['password'])
+
+    @jwt_app.identity_handler
+    def identify(payload):
+        user_id = payload['identity']
+        return services.get_user_by_username(user_id)
+
+    jwt_app.init_app(flask_app)
